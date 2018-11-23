384af95e7b000b1e91fae05f93b8388eec583b4f
sejinahn2 <43711842+sejinahn2@users.noreply.github.com>
2018-11-22 16:07:56 -0800

    Update NewtonsMethod.jl

--- src/NewtonsMethod.jl
+++ src/NewtonsMethod.jl
@@ -1,5 +1,33 @@
-module NewtonsMethod
+using ForwardDiff
 
-greet() = print("Hello World!")
+# operator to get the derivative of this function using AD
+D(g) = x -> ForwardDiff.derivative(g, x)
 
-end # module
+# example usage: create a function and get the derivative
+g(x) = (x-1)^3
+g_prime = D(g)
+
+using Plots, LinearAlgebra, Statistics
+function fixedpointmap(f, x_0, tolerance, maxiter)
+    # setup the algorithm
+    x_old = x_0
+    normdiff = Inf
+    iter = 1
+    while normdiff > tolerance && iter <= maxiter
+        x_new = f(x_old)  # use the passed in map
+        normdiff = norm(x_new - x_old)
+        x_old = x_new
+        iter = iter + 1
+    end
+    return (x_old, normdiff, iter)
+end
+
+# define a map and parameters
+f(v) = v-g(v)/g_prime(v) # note that p and β are used in the function!
+
+maxiter = 1000
+tolerance = 1.0E-7
+v_initial = 0.8 # initial condition
+
+v_star, normdiff, iter = fixedpointmap(f, v_initial, tolerance, maxiter)
+println("Fixed point = $v_star, and |f(x) - x| = $normdiff in $iter iterations")
